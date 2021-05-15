a59cd29274fca386c8402d6fed74c1be6c1fab9c
fumitofujiwara <ff2310527@icloud.com>
2021-05-15 14:47:41 +0900

    スケジュール投稿後エラーメッセージの表示

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -7,7 +7,7 @@ class Schedule < ApplicationRecord
   validate :start_ending_check
 
   def start_ending_check
-    errors.add(:ending_time, "は開始時間より遅い時間を選択してください。") if self.start_date > self.ending_date
+    errors.add(:ending_date, "終了時刻は開始時刻より遅い時間を選択してください。") if self.start_date > self.ending_date
   end
   
 end
