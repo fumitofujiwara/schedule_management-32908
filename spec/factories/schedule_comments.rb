94fd0606a22d42af47542dd8f42659e07193c9c8
fumitofujiwara <ff2310527@icloud.com>
2021-05-23 18:03:24 +0900

    コメント単体テストコードの記述

--- spec/factories/schedule_comments.rb
+++ spec/factories/schedule_comments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :schedule_comment do
-    
+    text {"テスト"}
+    association :user
+    association :schedule
   end
 end
--- spec/models/schedule_comment_spec.rb
+++ spec/models/schedule_comment_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe ScheduleComment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @schedule_comment = FactoryBot.build(:schedule_comment)
+  end
+  describe 'スケジュールコメント投稿' do
+    context 'コメントが投稿できるとき' do
+      it 'textが存在すれば投稿できる' do
+        expect(@schedule_comment).to be_valid
+      end
+    end
+    context 'コメントが投稿できないとき' do
+      it 'textがからの場合投稿できない' do
+        @schedule_comment.text = nil
+        @schedule_comment.valid?
+        expect(@schedule_comment.errors.full_messages).to include "Text can't be blank"
+      end
+      it 'userが紐づいていないと投稿できない' do
+        @schedule_comment.user = nil
+        @schedule_comment.valid?
+        expect(@schedule_comment.errors.full_messages).to include "User must exist"
+      end
+      it 'scheduleが紐づいていないと投稿できない' do
+        @schedule_comment.schedule = nil
+        @schedule_comment.valid?
+        expect(@schedule_comment.errors.full_messages).to include "Schedule must exist"
+      end
+    end
+  end
+
 end
