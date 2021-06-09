187118f7c98599f9b4d37084b0f72aba54ad28df
fumitofujiwara <ff2310527@icloud.com>
2021-06-09 21:31:06 +0900

    単体テストコードの記述

--- spec/factories/reports.rb
+++ spec/factories/reports.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :report do
-    
+    text {'イメージ'}
+    association :user
+    association :schedule
+
+    after(:build) do |report|
+      report.image.attach(io: File.open('public/images/test.png'), filename: 'test.png')
+    end
   end
 end
